b6e868e14c0a7fc619bd86f4cdfb9ee73dac5d18
jayd <jaydandekar3@gmail.com>
2023-04-23 00:42:58 +0530

    create Addition project

---
+++ Addition.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner; // Import the Scanner class
+
+class MyClass {
+    public static void main(String[] args) {
+        int x, y, sum;
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Type a number:");
+        x = myObj.nextInt(); // Read user input
+
+        System.out.println("Type another number:");
+        y = myObj.nextInt(); // Read user input
+
+        sum = x + y;  // Calculate the sum of x + y
+        System.out.println("Sum is: " + sum); // Print the sum
+    }
+}
